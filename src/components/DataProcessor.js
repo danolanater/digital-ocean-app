edafd0f46f49fd45cfc1d4be6bcda08c4bc16baf
danolanater <danolanater@gmail.com>
2021-02-16 10:24:06 -0800

    update data for new week and made styling improvements

--- src/components/DataProcessor.js
+++ src/components/DataProcessor.js
@@ -9,17 +9,25 @@ const NewWinners = dataAsArray.pop()
 /* Old Winners put in first index */
 dataAsArray.forEach(index => {
     if(index !== dataAsArray[0]) {
-        if(index.airpods) {
-            dataAsArray[0].airpods = (dataAsArray[0].airpods + "," + index.airpods).split(',')
+        if(index.AirPods) {
+            dataAsArray[0].AirPods = (dataAsArray[0].AirPods + "," + index.AirPods).split(',')
         }
-        if(index.ipad) {
-            dataAsArray[0].ipad = (dataAsArray[0].ipad + "," + index.ipad).split(',')
+        if(index.iPad) {
+            dataAsArray[0].iPad = (dataAsArray[0].iPad + "," + index.iPad).split(',')
         }
-        if(index.applewatch) {
-            dataAsArray[0].applewatch = (dataAsArray[0].applewatch + "," + index.applewatch).split(',')
+        if(index["Apple Watch"]) {
+            dataAsArray[0]["Apple Watch"] = (dataAsArray[0]["Apple Watch"] + "," + index["Apple Watch"]).split(',')
         }
-        if(index.homepod) {
-            dataAsArray[0].homepod = (dataAsArray[0].homepod + "," + index.homepod).split(',')
+        if(index.HomePod) {
+            dataAsArray[0].HomePod = (dataAsArray[0].HomePod + "," + index.HomePod).split(',')
+        }
+        if(index["HomePod mini"]) {
+            dataAsArray[0]["HomePod mini"] = (dataAsArray[0]["HomePod mini"] + "," + index["HomePod mini"]).split(',')
+            dataAsArray[0]["HomePod mini"].shift()
+        }
+        if(index.iPhone) {
+            dataAsArray[0].iPhone = (dataAsArray[0].iPhone + "," + index.iPhone).split(',')
+            dataAsArray[0].iPhone.shift()
         }
     }
 })
